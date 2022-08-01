a2b7bfc10bc7235d6df963e4f141c142700116b1
ahd3r <ander11110@gmail.com>
2022-08-01 22:28:36 +0300

    check tf subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,5 @@ resource "aws_subnet" "tf_subnet" {
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = true
   enable_resource_name_dns_a_record_on_launch = true
-  # enable_resource_name_dns_aaaa_record_on_launch = true
-  private_dns_hostname_type_on_launch = "ip-name" # resource-name
+  private_dns_hostname_type_on_launch = "ip-name"
 }
