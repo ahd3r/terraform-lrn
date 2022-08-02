12dc682c4097a616ea7b00d379cdde33ccecafcf
ahd3r <ander11110@gmail.com>
2022-08-02 16:16:06 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,3 +43,53 @@ resource "aws_default_route_table" "tf_rt" {
     gateway_id = aws_internet_gateway.tf_ig.id
   }
 }
+
+resource "aws_security_group" "tf_sg" {
+  name = "sg"
+  description = "Open server ports"
+  vpc_id = aws_vpc.tf_vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 2
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "tf_key_pair" {
+  key_name   = "tf_key_pair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8y4NucoBq04rwNkIUu4krEJ4zgRDpjNBueEh2S6coTcYS3++ZnCOJic+STQEXutezD6I82L6mP4FozUNA1l0do6m+yEsTHI8ssvy5wgT+XmP3m36tr0zA9l+albEg+aEZVpmyUl6gN/D6QQavO1BscYk3YqnVahgGt35UsOkTrzH9M9uhI5eokwLMVi3gYlUc2oknPKYkgW/92WljKTmZbGCtUUPyf7OdqW4x8dS6c4qp2oiT2Tg7wreDKPHx4myLnqb6INNlUGzszS2PeOsq9KukY443oUmVAsLZjpLX3GOOMljkwa9KxSubZhkkU/8QghmAtB8mMVEeqdSIWklLo9cj/TdqIvposeVGU1uL8+z3wUGuK0wDk84HERpH2z/Lqaber48l3vnFBP2YRaLIQh98A3f3KPfyGv9YEne6dBBTX9+I3gsj0KGU62o8OMxh0UsrcRL85eA5R0CGqbqKu+VrxpsU9Re7vNxxK4GBrNKyHkvcYN0+earEGhb0+8k= ander@ander-Inspiron-5584"
+}
+
+resource "aws_instance" "tf_test_ec2" {
+  ami = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  disable_api_termination = true
+  vpc_security_group_ids = [aws_security_group.tf_sg.id]
+  key_name = "tf_key_pair"
+}
