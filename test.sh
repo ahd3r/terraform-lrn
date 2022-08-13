7bb249c05df381c9bf5cbd89fc361068bd9364f6
ahd3r <ander11110@gmail.com>
2022-08-13 12:56:17 +0300

    try to check status

--- test.sh
+++ test.sh
@@ -1,16 +1,13 @@
-# #!/bin/bash
+#!/bin/bash
 cd terraform
 
 resources=$(terraform state list)
-echo $(terraform state list)
-echo 1 $resources
-echo 2 "$resources"
-echo 3 "${resources}"
-IFS=$'\n' read -r -a array <<< "${resources}"
+echo $resources
+# IFS=$'\n' read -r -a array <<< "${resources}"
 
-for i in "${array[@]}";
-do
-  echo "bla bla $i"
-done
+# for i in "${array[@]}";
+# do
+#   echo "bla bla $i"
+# done
 
-terraform state show aws_instance.tf_test_ec2
+# terraform state show aws_instance.tf_test_ec2
